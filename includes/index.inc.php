74aeef4032ab6b44ce47d7d7c50e95ee665faff4
Hurmatullah <hrasaa1@gmail.com>
2022-07-09 21:56:19 +0300

    Added some comments

--- includes/index.inc.php
+++ includes/index.inc.php
@@ -15,7 +15,11 @@ class ParentClass{
 
 class ChildClass{
 
+ // Private access it means that you can only access it inside of class not anywhere else
+ // Protected access modifier is that you can only access it in inside of class or another class that extends that class as well.
 
+
+	
   private function owner(){
 
   	$a = "this is a simple text";
