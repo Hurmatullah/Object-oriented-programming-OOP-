51404756326b12de84bc931d9ac9d4a619cff056
Hurmatullah <hrasaa1@gmail.com>
2022-07-10 01:33:42 +0300

    Update

--- includes/index.inc.php
+++ includes/index.inc.php
@@ -6,23 +6,23 @@ class ParentClass{
 
 	// properties or methods
 
-	private $name = "Hurmat";
+	protected $name = "Hurmat";
 	private $fatherName = "Hamed";
 	private $suject = "Mathmatic";
 
 }
 
 
-class ChildClass{
+class ChildClass extends ParentClass{
 
  // Private access it means that you can only access it inside of class not anywhere else
  // Protected access modifier is that you can only access it in inside of class or another class that extends that class as well.
 
 
-	
-  private function owner(){
 
-  	$a = "this is a simple text";
+  public function owner(){
+
+  	$a = $this->name;
 
   	return $a;
 
