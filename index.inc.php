f0476b26792131010a06cdfc59dc135d7f1fccb9
Hurmatullah <hrasaa1@gmail.com>
2022-07-09 20:56:06 +0300

    First commit

---
+++ index.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+	
+class NewClass{
+
+	// properties and methods will be here
+
+	public $text = "Hello world";
+
+
+}
+
+
+$objects = new NewClass;
+var_dump($objects);	
+
+
+
+
+
+?>
+
+
+
+
