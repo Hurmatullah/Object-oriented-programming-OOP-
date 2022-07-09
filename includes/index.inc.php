f8957d205bc793a62af61a06a110cd368fa9d593
Hurmatullah <hrasaa1@gmail.com>
2022-07-09 21:54:09 +0300

    Updated code

---
+++ includes/index.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+	
+class ParentClass{
+
+
+	// properties or methods
+
+	private $name = "Hurmat";
+	private $fatherName = "Hamed";
+	private $suject = "Mathmatic";
+
+}
+
+
+class ChildClass{
+
+
+  private function owner(){
+
+  	$a = "this is a simple text";
+
+  	return $a;
+
+  }
+
+
+
+}
+
+
+
+
+
+?>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include "includes/index.inc.php";
+
+?>
+
+<html>
+<head></head>
+<body>
+
+
+	<?php
+
+	$object = new childClass();
+
+	 echo $object->owner();
+
+
+	?>
+
+
+
+</body>
+</html>
+
