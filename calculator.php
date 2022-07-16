cf00e43cb8ae442cd0238e19e34311d880cc31a2
Hurmatullah <hrasaa1@gmail.com>
2022-07-16 09:41:30 -0700

    Calculator class created

---
+++ calculator.php
@@ -0,0 +1,28 @@
+<html>
+
+<head></head>
+
+<body>
+
+    <h2>Simple calculator</h2>
+
+    <form action="" method="post">
+
+        <label for="number1">Number 1</label>
+        <input type="number" name="num1" /><br>
+        <label for="number2">Number 2</label>
+        <input type="number" name="num2" /><br>
+        <select name="operation" id="">
+            <option value="add">Add</option>
+            <option value="sub">Substarct</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select><br>
+
+        <button type="submit">Submit</button>
+    </form>
+
+
+</body>
+
+</html>
