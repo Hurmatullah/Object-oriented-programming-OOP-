37f33d3cd075c2e64ef3551d2ed10766c7e11aa1
Hurmatullah <hrasaa1@gmail.com>
2022-07-17 10:50:28 -0700

    Updated code

--- classes_main/payment.class.php
+++ classes_main/payment.class.php
@@ -10,40 +10,11 @@ interface LoginMethod
     public function LoginFirst();
 }
 
-
-
-class Paypal implements PaymentMethod, LoginMethod
-{
-    public function LoginFirst()
-    {
-    }
-    public function pay()
-    {
-    }
-    public function paymentProcess()
-    {
-        $this->LoginFirst();
-        $this->pay();
-    }
-}
-
 class Visa implements PaymentMethod
 {
     public function pay()
     {
     }
-    public function paymentProcess()
-    {
-        $this->pay();
-    }
-}
-
-class Cash implements PaymentMethod
-{
-    public function pay()
-    {
-    }
-
     public function paymentProcess()
     {
         $this->pay();
@@ -57,8 +28,3 @@ class BuyProduct
         $paymentType->paymentProcess();
     }
 }
-
-
-$paymentType = new Cash();
-$buyProduct = new BuyProduct();
-$buyProduct->payNow($paymentType);
