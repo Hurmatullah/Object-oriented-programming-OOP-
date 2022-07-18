c5d9e8aba12822f2e4bcd85eb00fb8afce143868
Hurmatullah <hrasaa1@gmail.com>
2022-07-18 01:34:35 -0700

    Connection of database created

--- classes/db.class.php
+++ classes/db.class.php
@@ -3,4 +3,18 @@
 
 class DB
 {
+
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "";
+    private $dbName = "mvc";
+
+    protected function connect()
+    {
+
+        $dsn = "mysql:host" . $this->host . ";dbName" . $this->dbName;
+        $connection = new PDO($dsn, $this->user, $this->password);
+        $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $connection;
+    }
 }
